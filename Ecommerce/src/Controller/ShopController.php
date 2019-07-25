1e61df46448b58cd1f08a0ec1f2db7b3c6145d39
Tanguy <Tanguy.duw@gmail.com>
2019-07-25 10:28:00 +0200

    Affichage obj BDD + mis en place route "show"

--- Ecommerce/src/Controller/ShopController.php
+++ Ecommerce/src/Controller/ShopController.php
@@ -2,19 +2,43 @@
 
 namespace App\Controller;
 
+
+use App\Repository\PropertyRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Property;
 
 class ShopController extends AbstractController
 {
+
+    public function __construct(PropertyRepository $repository, ObjectManager $em){
+        $this->repository = $repository;
+        $this->em = $em;
+    }
+
     /**
      * @Route("/shop", name="shop")
+     *
      */
     public function index()
     {
+       $property = $this->repository->findAll();
         return $this->render('shop/index.html.twig', [
             'current_menu' => 'Shop',
+            'properties' => $property
+        ]);
+    }
+
+    /**
+     *@Route("/shop/{slug}-{id}", name="property.show", requirements={"slug": "[a-z0-9\-]*"})
+     */
+    public function show($slug, $id): response {
+        $property = $this->repository->find($id);
+        return $this->render('shop/show.html.twig', [
+            'property' => $property,
+            'current_menu' => 'Shop'
         ]);
     }
 }
--- Ecommerce/src/Entity/Property.php
+++ Ecommerce/src/Entity/Property.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Cocur\Slugify\Slugify;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\PropertyRepository")
@@ -26,6 +27,36 @@ class Property
      */
     private $description;
 
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $prix;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $review;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $rate;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $categorie;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    public function __construct()
+    {
+        $this->created_at = new \DateTime();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -43,6 +74,11 @@ class Property
         return $this;
     }
 
+    public function getSlug(): string
+    {
+       return (new Slugify())->slugify($this->name);
+    }
+
     public function getDescription(): ?string
     {
         return $this->description;
@@ -51,6 +87,71 @@ class Property
     public function setDescription(?string $description): self
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrix(): ?int
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(int $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getFormattedPrix(): string
+    {
+        return number_format($this->prix,0,'', ' ');
+    }
+
+    public function getReview(): ?string
+    {
+        return $this->review;
+    }
+
+    public function setReview(?string $review): self
+    {
+        $this->review = $review;
+
+        return $this;
+    }
+
+    public function getRate(): ?int
+    {
+        return $this->rate;
+    }
+
+    public function setRate(?int $rate): self
+    {
+        $this->rate = $rate;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?string
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(?string $categorie): self
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
 
         return $this;
     }
